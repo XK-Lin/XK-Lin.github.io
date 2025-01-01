a7298b08f479e2a465afec800bbebbcbc0bda955
Xukun Lin <linxk123@gmail.com>
2024-12-31 19:16:56 -0800

    update

---
+++ script.js
@@ -0,0 +1,15 @@
+const sidebarButton = document.getElementById("sidebar-button");
+const sidebar = document.querySelector("ul");
+sidebarButton.addEventListener("click", toggleSidebar);
+function toggleSidebar() {
+    if (sidebarButton.innerHTML === '<i class="fa-solid fa-bars"></i>') {
+        sidebarButton.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+        sidebar.style.width = "calc(150px + 5vw)";
+    } else {
+        reset();
+    }
+}
+function reset() {
+    sidebarButton.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    sidebar.style.width = "0";
+}
